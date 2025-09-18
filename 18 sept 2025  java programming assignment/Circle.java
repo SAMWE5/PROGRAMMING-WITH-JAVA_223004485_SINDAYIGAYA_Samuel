dfc747dded79196dfbbdb4a534595b2ba97100ff
SAMWE5 <sindayigayasamuel38@gmail.com>
2025-09-18 18:21:36 +0200

    Update Circle.java

--- 18 sept 2025  java programming assignment/Circle.java
+++ 18 sept 2025  java programming assignment/Circle.java
@@ -1,17 +1,52 @@
-package Casestudy4;
+//SINDAYIGAYA SAMUEL 223004485
+//NSABIYUMVA JEAN MARIE VIANNEY 223008033
+package Rectcalculator;
 
-public class Circle {
+import java.util.Scanner;
 
-
-	private double radius;
-
-	// Constructor
-	public Circle(double radius) {
-		this.radius = radius;
-	}
-
-	// Method to calculate area
-	public double calculateArea() {
-		return Math.PI * radius * radius;
-	}
+public class circle {
+private double radius;
+public void Circle(double radius) {
+    this.radius = radius;
 }
+
+public double calculateArea() {
+    return Math.PI * radius * radius;
+}
+
+public double calculateCircumference() {
+    return 2 * Math.PI * radius;
+}
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Enter the radius of the circle: ");
+    double radius = scanner.nextDouble();
+    
+    circle circle = new circle();
+    
+    System.out.println("Choose an option:");
+    System.out.println("1. Calculate Area");
+    System.out.println("2. Calculate Circumference");
+    System.out.print("Enter your choice: ");
+    
+    int choice = scanner.nextInt();
+    
+    switch (choice) {
+        case 1:
+            System.out.println("Area: " + circle.calculateArea());
+            break;
+        case 2:
+            System.out.println("Circumference: " + circle.calculateCircumference());
+            break;
+        default:
+            System.out.println("Invalid choice!");
+    }
+    
+    scanner.close();
+}
+}
+
+
+
