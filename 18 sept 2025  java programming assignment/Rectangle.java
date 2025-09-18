f9364d2c2689a09731628ff22cd745cbc3158e96
SAMWE5 <sindayigayasamuel38@gmail.com>
2025-09-18 18:09:12 +0200

    Update Rectangle.java

--- 18 sept 2025  java programming assignment/Rectangle.java
+++ 18 sept 2025  java programming assignment/Rectangle.java
@@ -1,11 +1,44 @@
+// SINDAYIGAYA Samuel 223004485
+//NSABIYUMVA Jean Marie 223008033
+package Rectcalculator;
+
+import java.util.Scanner;
 
 public class Rectangle {
-
-   public static void main(String[] args) {
-       int Length= 10;
-	   int Width=7;
-	   System.out.println("AREA OF RECTANGLE= Length*Width");
-	   System.out.println("PERIMETER OF RECTANGLE=2*(Length+Width");
-
+	private double length;
+	private double width;
+	public Rectangle(double length,double width){
+		this.length=length;
+		this.width=width;
+		
 	}
+	public double calculateArea(){
+		return length*width;
+		
+		}
+	public boolean isSquare(){
+		return length==width;
+		
+	}
+	public static void main(String[] args) {
+		Scanner Sc =new Scanner(System.in);
+	System.out.println("Enter the length of Rectangle: ");
+	 
+	double length=Sc.nextDouble();
+	System.out.println("Enter the width of Rectangle: ");
+	double width = Sc.nextDouble();
+	Rectangle rectangle = new Rectangle(length, width);
+	double Area = rectangle.calculateArea();
+	System.out.println("Area of rectangle: "+ Area);
+	if(rectangle.isSquare()){
+		System.out.println("this rectangle is a square.");
+		
+	}else{
+		System.out.println("this rectangle is not a square.");
+		
+	}
+	System.out.println("thank you for using the program.");
+	Sc.close();
+	}
+
 }
