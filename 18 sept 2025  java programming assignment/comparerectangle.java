36519ac97d548a5c92b61c5deaff5198fc5fad9c
SAMWE5 <sindayigayasamuel38@gmail.com>
2025-09-26 06:22:37 +0200

    Update comparerectangle.java

--- 18 sept 2025  java programming assignment/comparerectangle.java
+++ 18 sept 2025  java programming assignment/comparerectangle.java
@@ -1,5 +1,6 @@
-// SINDAYIGAYA Samuel 223004485
-//NSABIYUMVA Jean Marie 223008033
+//SINDAYIGAYA SAMUEL 223004485
+//NSABIYUMVA JEAN MARIE VIANNEY 223008033
+//IRADUKUNDA Epiphanie 223015618
 import java.util.Scanner;
 public class comparerectangle {
 	 private double length;
@@ -66,4 +67,5 @@ public class comparerectangle {
 	    }
 	}
 	
+
 
