ac5ded1d5c575659773535c1c228f8bd10d273a6
SAMWE5 <sindayigayasamuel38@gmail.com>
2025-09-27 12:41:52 +0200

    Update PaymentsPanel.java

--- 26septemberJAVA group/PaymentsPanel.java
+++ 26septemberJAVA group/PaymentsPanel.java
@@ -1,4 +1,6 @@
-package com.SFMS;
+//SINDAYIGAYA SAMUEL 223004485
+//NSABIYUMVA JEAN MARIE VIANNEY 223008033
+//IRADUKUNDA Epiphanie 223015618package com.SFMS;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -301,4 +303,5 @@ public class PaymentsPanel extends JPanel {
         statusComboBox.setSelectedIndex(0);
         paymentsTable.clearSelection();
     }
+
 }
